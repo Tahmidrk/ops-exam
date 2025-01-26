50d675c76d564f75bbe24192e0170b3440f412c0
Tahmidrk <23524202041@student.bup.edu.bd>
2025-01-27 04:14:51 +0600

    Update Exception2.java

--- Java_Lab-3/Exception2.java
+++ Java_Lab-3/Exception2.java
@@ -1,1 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+     try{
+         int number[]=new int[10];
+         System.out.println("Print:"+number[11]);
+         number[11]=30/0;
+         int num=30/0;
+     }
+     catch (ArithmeticException |ArrayIndexOutOfBoundsException e){
+         System.out.println("Error message:"+e);
+     }
+//     catch (ArrayIndexOutOfBoundsException e){
+//         System.out.println("Index out of size of the array");
+//     }
+      
+     }
+    }
 
