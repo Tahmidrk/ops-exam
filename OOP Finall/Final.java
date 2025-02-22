64706509c78a2123648c17e4240810cc687a451f
Tahmidrk <23524202041@student.bup.edu.bd>
2025-02-23 04:24:45 +0600

    Create Final.java

---
+++ OOP Finall/Final.java
@@ -0,0 +1,20 @@
+class Student1{
+    final int rollNumber;
+    Student1(int rollNumber){
+        this.rollNumber = rollNumber;
+    }
+    void display(){
+        System.out.println("Roll Number:"+rollNumber);
+    }
+}
+public class Final{
+    public static void main(String[] args) {
+        Student1 s1=new Student1(145);
+        Student1 s2=new Student1(131);
+        s1.display();
+        s2.display();
+    }
+}
+/*output:
+Roll Number:145
+Roll Number:131*/
